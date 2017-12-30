60f979b7756c8890a4647b30985719f39f1d8094
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-29 21:53:08 -0300

    CH02 - Javascript

--- ch02/js/01.js
+++ ch02/js/01.js
@@ -11,5 +11,30 @@ function declareConstModerWay()
     console.log("Empanadas: " + EMPANADAS);
 }
 
+function variableSinAmbito()
+{
+    var topic = "JavaScript";
+    if(topic)
+    {
+        // las llaves no bloquean el alcance de una variable.
+        var topic = "React"; // esta variable en el bloque if restablece el valor de topic a nivel function
+        console.log("block: ", topic); // block React
+    }
+    console.log('global: ', topic); // block React
+}
+
+function variableConAmbito()
+{
+    var topic = "JavaScript";
+    if(topic)
+    {
+        let topic = "React"; // let protege el valor de la variable global topic
+        console.log('block: ', topic);
+    }
+    console.log('global: ', topic);
+}
+
+variableConAmbito();
+variableSinAmbito();
 declareConstOldWay();
 declareConstModerWay();
